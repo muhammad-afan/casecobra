e5b88d8001d7d5d1cc030767371b9320d08ebe43
M-Afan <afansaeed19971719@gmail.com>
2024-07-20 17:18:56 +0500

    landing paged added

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,17 +1,17 @@
 import { cn } from "@/lib/utils";
 
 const MaxWidthWrapper = ({
-  classname,
+  className,
   children,
 }: {
-  classname?: string;
+  className?: string;
   children: React.ReactNode;
 }) => {
   return (
     <div
       className={cn(
         "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
-        classname
+        className
       )}
     >
       {children}
