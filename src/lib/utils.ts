8b2f0fb58287c7675c732d0feb01f79226df7a47
M-Afan <afansaeed19971719@gmail.com>
2024-07-29 21:28:41 +0500

    chore: Update metadata construction in layout.tsx and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Metadata } from "next";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,3 +13,34 @@ export const FormatPrice = (price: number) => {
   });
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thubmnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@afan", // twitter username
+    },
+    icons,
+    metadataBase: new URL("https://casecobra-nine-xi.vercel.app"),
+  };
+}
